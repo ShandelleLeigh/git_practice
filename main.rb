95c8f63fd044522094a4b318ea6ca7c0343ebf83
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-02-09 11:56:31 -0700

    changes indentation

--- main.rb
+++ main.rb
@@ -2,25 +2,26 @@ require 'colorize'
 
 def puts_git(cmd)
     puts `git #{cmd} -h`
+end
   
-  def menu
-    puts 'Main Menu:' .colorize(:CYAN)
-    puts '1: Enter git command' .colorize(:CYAN)
-    puts '2: Exit'.colorize(:CYAN)
-    choice = gets.to_i
-    case choice
-      when 1
+def menu
+        puts 'Main Menu:' .colorize(:CYAN)
+        puts '1: Enter git command' .colorize(:CYAN)
+        puts '2: Exit'.colorize(:CYAN)
+        choice = gets.to_i
+case choice
+    when 1
         puts 'Enter git command'.colorize(:green)
         puts_git(gets.strip)
         menu
-      when 2
-        exit
-      else
-        puts 'Invalid choice'.colorize(:red)
-        menu
-      end
+    when 2
+    
+    else
+    puts 'Invalid choice'.colorize(:red)
+        
     end
 end
+
 
 
 menu
